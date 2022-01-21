704071c3edeac07ad14407e6ce5c034974e90c86
sahil <vora.sahil01@gmail.com>
2022-01-22 02:00:18 +0530

    If else question soluton

---
+++ Introduction/IfElse/solution.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 != 0:
+    print("Weird")
+elif n >= 2 and n <= 5:
+    print("Not Weird")
+elif n >= 6 and n<= 20:
+    print("Weird")
+else:
+     print("Not Weird")
